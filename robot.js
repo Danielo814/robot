3368379b60c573364b6a5b005631d69e9da3a277
Danielo814 <ojedadaniel92@gmail.com>
2019-11-15 13:53:29 -0800

    choose new place on map to arrive at based on direction

--- robot.js
+++ robot.js
@@ -88,3 +88,7 @@ function randomPick(array) {
   let choice = Math.floor(Math.random() * array.length);
   return array[choice];
 }
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
